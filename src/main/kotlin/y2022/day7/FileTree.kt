57f80edc2923f768a542a241e4f3e0444a5c6363
Roman Mertyn <r.mertyn@betterbits.de>
2022-12-07 18:38:59 +0100

    Day 7 - finally solution

--- src/main/kotlin/y2022/day7/FileTree.kt
+++ src/main/kotlin/y2022/day7/FileTree.kt
@@ -2,27 +2,17 @@ package y2022.day7
 
 import tools.readListOfStringFromInput
 
-sealed class Node(val name: String, val level: String) {
-    abstract fun size():Int
-    override fun toString(): String {
-        return """
-            |${level}name: $name                
-            """.trimIndent()
-    }
-    class Dir(name: String, level: String) : Node(name, level){
-        val childs : MutableList<Node> = mutableListOf()
-        override fun size(): Int {
-            return if(childs.isEmpty()) 0 else childs.sumOf { it.size() }
-        }
-        override fun toString(): String {
-            return """
-            |${level}name: $name
-            ${childs.joinToString(separator = "\n"){it.toString()}}
-            """.trimIndent()
-        }
+sealed class Node(val name: String, val parentNode: Dir? = null) {
+    abstract fun size(): Int
 
+    class Dir(name: String, parentNode: Dir? = null) : Node(name, parentNode) {
+        val childs: MutableList<Node> = mutableListOf()
+        override fun size(): Int {
+            return if (childs.isEmpty()) 0 else childs.sumOf { it.size() }
+        }
     }
-    class File(name: String, level: String, private val size: Int) : Node(name, level){
+
+    class File(name: String, private val size: Int, parentNode: Dir) : Node(name, parentNode) {
         override fun size(): Int {
             return size
         }
@@ -31,41 +21,89 @@ sealed class Node(val name: String, val level: String) {
 
 fun main() {
     val exampleInput = readListOfStringFromInput("y2022/day7/example.txt")
+    check(findSizeOfRemovalCandidates1(exampleInput) ==95437)
+    
+    val input = readListOfStringFromInput("y2022/day7/input1.txt")
+    println(findSizeOfRemovalCandidates1(input))
 
-// Root
-    val root = exampleInput[0].toNode()
-    parse(parentNode = root, exampleInput.subList(1, exampleInput.size))
-    println(root)
+    val candidateToFreeSizeInExample = findBestCandidateToFreeSize(exampleInput)
+    check(candidateToFreeSizeInExample.name == "d")
+    
+    val candidateToFreeSizeInTest = findBestCandidateToFreeSize(input)
+    println(candidateToFreeSizeInTest.size())
+    
 }
 
+fun findBestCandidateToFreeSize(input: List<String>): Node.Dir {
+    val rootNode = parse(input)
+    val sortedBy = traverseWithoutMaxSize(rootNode).sortedBy { it.size() }
+    val currentlyUsed = sortedBy.last().size()
+    return sortedBy.filter { 70_000_000 - currentlyUsed + it.size() >= 30_000_000 }.minBy { it.size() }
+}
 
-fun parse(parentNode: Node, sublist:List<String>):Node{
-    if (parentNode is Node.Dir && sublist.isNotEmpty()){
-        // Find all entries of same level and split into sub lists
-        val indicies = sublist.filter { it.startsWith(parentNode.level + "-") }.map { sublist.indexOf(it) }
-        indicies.windowed(2, 1, partialWindows = true).forEach { window ->
-            println("Window $window")
-            val start = window[0]
-            val end = if(window.size == 2)window[1]else sublist.size
-            val node = sublist[start].toNode()
-            if(node is Node.Dir)
-            {
-                val subSubList = sublist.subList(start+1, end)
-                parse(node, subSubList)
-            }
-            (parentNode).childs.add(node)
-        }
+fun traverseWithoutMaxSize(current: Node.Dir): List<Node.Dir> {
+    val response = mutableListOf<Node.Dir>(current)
+    for (childDir in current.childs.filterIsInstance<Node.Dir>()) {
+        response.addAll(traverseWithoutMaxSize(childDir))
     }
-    return parentNode
+    return response;
 }
-fun String.toNode(): Node {
-    fun getSize():Int = this.substringAfter("size=").substringBefore(")").toInt()
-    val name = this.substringAfter("-").substringBefore("(").trim()
-    val level = this.takeWhile { it == ' ' } + "  "
-    return if (this.endsWith("(dir)")) {
-        Node.Dir(name, level)
+
+
+fun findSizeOfRemovalCandidates1(input: List<String>): Int{
+    val node = parse(input)    
+    return traverse(node).sumOf { it.size() }
+}
+fun traverse(current: Node.Dir): List<Node.Dir> {
+    val response = mutableListOf<Node.Dir>()
+    for (childDir in current.childs.filterIsInstance<Node.Dir>()) {        
+        response.addAll(traverse(childDir).filter { it.size() < 100_000 })
+    }
+    if(current.size() < 100_000) response.add(current)
+    return response;
+}
+
+fun parse(list: List<String>): Node.Dir {
+    var current: Node.Dir = Node.Dir("/")
+    var index = 0
+    do {
+        val line = list[index]
+        when{
+            line.startsWith("$ cd /") -> current = navigateToRoot(current)
+            line.startsWith("$ cd ..") -> current = navigateToParent(current)
+            line.startsWith("$ cd ") -> current = navigateToChild(current, line)
+            line.startsWith("$ ls") -> index = parseListOutput(current, list, index)
+        }
+    }while (++index <list.size)
+    return navigateToRoot(current) 
+}
+
+fun parseListOutput(current: Node.Dir, list: List<String>, index: Int): Int {
+    var localIndex = index + 1
+    do {
+        current.childs.add(list[localIndex].toNode(current))        
+    }while (++localIndex < list.size && !list[localIndex].startsWith("$"))
+    return localIndex - 1
+}
+fun navigateToChild(current: Node.Dir, line: String): Node.Dir {
+    val childName = line.substring(5, line.length)
+    return current.childs.filterIsInstance<Node.Dir>().find { it.name == childName }?:current
+}
+fun navigateToParent(current: Node.Dir): Node.Dir {
+    return current.parentNode?.let { current.parentNode } ?: current
+}
+fun navigateToRoot(current: Node.Dir): Node.Dir {
+    return if(current.parentNode == null) current
+    else navigateToRoot(current.parentNode)
+}
+
+fun String.toNode(current: Node.Dir): Node {
+    fun getSize(): Int = this.substringBefore(" ").toInt()
+    val name = this.substringAfter(" ").trim()    
+    return if (this.startsWith("dir")) {
+        Node.Dir(name, current)
     } else {
-        Node.File(name, level, getSize())
+        Node.File(name, getSize(), current)
     }
 }
 
